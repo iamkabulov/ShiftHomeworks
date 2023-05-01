7ac2d2673094c5acd2bfafb90992a5778efefc05
kabulov <n.kabulov@cft.ru>
2023-05-01 19:17:15 +0600

    Сверстал view и добавил во viewController

---
+++ homework-3/homework-3/SkillScreen/DeveloperSkillsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DeveloperSkillsViewController.swift
+//  homework-3
+//
+//  Created by Кабулов Нурсултан Пернебаевич on 27.04.2023.
+//
+
+import UIKit
+
+class DeveloperSkillsViewController: UIViewController {
+
+	private let skillsView = DeveloperSkillsView()
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		style()
+		skillsView.setDate(model: developerSkillsModel)
+	}
+
+
+}
+
+extension DeveloperSkillsViewController {
+	func style() {
+		skillsView.translatesAutoresizingMaskIntoConstraints = false
+
+		view.addSubview(skillsView)
+		NSLayoutConstraint.activate([
+			skillsView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+			skillsView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			skillsView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+			skillsView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+		])
+	}
+}

a67f8532fce7e9ccc19f9e889d4b8f5840d3b541
kabulov <n.kabulov@cft.ru>
2023-05-03 17:14:08 +0600

    Подправил отступы во вью моделях

--- homework-3/homework-3/SkillScreen/DeveloperSkillsViewModel.swift
+++ homework-3/homework-3/SkillScreen/DeveloperSkillsViewModel.swift
@@ -17,16 +17,15 @@ struct DeveloperSkillsViewModel {
 	static func defaultViewModel() -> DeveloperSkillsViewModel {
 		return DeveloperSkillsViewModel(languageIcon: UIImage(systemName: "swift", withConfiguration: UIImage.SymbolConfiguration(paletteColors: [.orange])) ?? UIImage(),
 										languageName: "Swift",
-												 learningDuration: "Изучаю 1 год",
-												 skills: """
-														 \u{2022} Владею базовыми командами git. (pull, push, merge, rebase и тд.) \n
-														 \u{2022} Пишу UI тесты (XCTest) \n
-														 \u{2022} Использую паттерн MVC \n
-														 \u{2022} Умею дебажить код \n
-														 \u{2022} Владею snapKit \n
-														 \u{2022} Знаю базовые компоненты UIKit \n
-
-														 """,
-												 wish: "На этом курсе хочу получить углубленные знания и практический опыт, а также начать карьеру в разработке")
+										learningDuration: "Изучаю 1 год",
+										skills: """
+										\u{2022} Владею базовыми командами git. (pull, push, merge, rebase и тд.) \n
+										\u{2022} Пишу UI тесты (XCTest) \n
+										\u{2022} Использую паттерн MVC \n
+										\u{2022} Умею дебажить код \n
+										\u{2022} Владею snapKit \n
+										\u{2022} Знаю базовые компоненты UIKit \n
+										""",
+										wish: "На этом курсе хочу получить углубленные знания и практический опыт, а также начать карьеру в разработке")
 	}
 }

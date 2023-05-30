9830e397f92e181131ecefc89a3d5d94df636b5e
kabulov <n.kabulov@cft.ru>
2023-05-30 14:06:58 +0600

    добавил entity и router

---
+++ homework-7/homework-7/ImagesListEntity.swift
@@ -0,0 +1,25 @@
+//
+//  ImagesEntity.swift
+//  homework-7
+//
+//  Created by Кабулов Нурсултан Пернебаевич on 26.05.2023.
+//
+
+import UIKit
+
+
+struct ImagesListEntity {
+	let url: String
+	var image: UIImage?
+	var isLoaded: Bool {
+		get {
+			if image != nil {
+				return true
+			}
+			else {
+				return false
+			}
+		}
+	}
+	var isPaused: Bool?
+}

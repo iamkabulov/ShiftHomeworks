0fabe0c8b4197aa403ce529e17b6d3ab984578f6
kabulov <n.kabulov@cft.ru>
2023-05-30 14:07:42 +0600

    Настроил AppDelegate

--- homework-7/homework-7/AppDelegate.swift
+++ homework-7/homework-7/AppDelegate.swift
@@ -15,7 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
 		window = UIWindow(frame: UIScreen.main.bounds)
 		window?.makeKeyAndVisible()
-		window?.rootViewController = ViewController()
+		let router = ImagesListRouter()
+		let interactor = ImagesListInteractor()
+		let presenter = ImagesListPresenter(router: router)
+		presenter.interactor = interactor
+		interactor.presenter = presenter
+		let vc = ImagesListViewController(presenter: presenter)
+		let nav = UINavigationController(rootViewController: vc)
+		nav.navigationBar.prefersLargeTitles = true
+		window?.rootViewController = nav
 
 		return true
 	}

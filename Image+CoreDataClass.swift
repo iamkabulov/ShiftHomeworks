2f07377cfa9d83845f55fe8efb0f01511002c106
kabulov <n.kabulov@cft.ru>
2023-06-08 23:27:24 +0600

    Добавил модель

---
+++ Image+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Image+CoreDataClass.swift
+//  homework-7
+//
+//  Created by Кабулов Нурсултан Пернебаевич on 08.06.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Image: NSManagedObject {
+
+}
